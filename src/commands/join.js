f94d82d168d827de6e05b5e7a62d014a32e5887e
Ryan Harrington <rharrington.web@gmail.com>
2020-06-04 14:42:54 -0600

    feat: join command reaction on success

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,4 +1,11 @@
+const joinChannel = (msg, channel) => {
+    channel.join()
+    return msg.react('👍')
+}
+
 module.exports = (msg, client, args) => {
     const channel = msg.member.voice.channel
-    channel.join()
+    return (channel)
+        ? joinChannel(msg, channel)
+        : msg.reply('You are not connected to a voice channel!')
 }
